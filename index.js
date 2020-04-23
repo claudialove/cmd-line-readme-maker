eeebdc40ac9380f491d28149497dfb7b1bd29a4c
Claudia Petty <claudialove32@gmail.com>
2020-04-23 19:46:24 -0400

    objects declared, prompts written, and output varible defined with a call to writeToFile - but i still need to write to the write to file function

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 var axios = require('axios');
-
-//prompts for user input
-inquirer.prompt([
+const questions = [ 
     {
         type: "input",
         name: "gitUserName",
@@ -63,30 +61,39 @@ inquirer.prompt([
         message: "What does the user need to know about contributing to the repo?"
     }
 
-]).then(function(data) {
-    console.log(data.gitUserName)
-    console.log(data.gitAssociatedEmail)
-    console.log(data.gitProjectUrl)
-    console.log(data.gitProjectName)
-    console.log(data.gitProjectDesc)
-    console.log(data.activities)
-    console.log(data.license)
-    console.log(data.installDepends)
-    console.log(data.runTests)
-    console.log(data.aboutRepo)
-    console.log(data.contributeToRepo)
-  
-    });
-    
-
-
-/*// -- They give us an ARRAY called 'questions' What could we do with this (?) -- //
-const questions = [
-
 ];
 
+//prompts for user input
+inquirer.prompt().then(function(data) {
+    //let output = empty string
+    let output = "";
+    output += `# ${data.gitProjectName}\n`
+    output += "\n"
+    output += `## ${data.gitProjectDesc}`
+    output += "\n"
+    output += `## ${data.gitUserName}`
+    output += "\n"
+    output += `## ${data.gitAssociatedEmail}`
+    output += "\n"
+    output += `## ${data.gitProjectUrl}`
+    output += "\n"
+    output += `## ${data.activities}`
+    output += "\n"
+    output += `## ${data.license}`
+    output += "\n"
+    output += `## ${data.installDepends}`
+    output += "\n"
+    output += `## ${data.runTests}`
+    output += "\n"
+    output += `## ${data.aboutRepo}`
+    output += "\n"
+    output += `## ${data.contributeToRepo}`
+  writeToFile("reame.md", output)
+    });
+    
 // -- They give us a writeToFile() FUNCTION, Looks like we may need to read/write to a file. What BUILT-IN node module will help us out with this (?) -- // 
 function writeToFile(fileName, data) {
+    // use fs to output the (google fs write to file)
 }
 
 // -- This is a fairly common programming construct. They are just giving us a FUNCTION to INITIALIZE or SETUP our project parameter. It's also where we usually kick off our project flow -- //
@@ -101,18 +108,30 @@ init();
 //adding possible algorithim for workflow if this program ends up working as expected to understand where in the process it gets called once it exists
 
 
-//approach 1
-//step 1: user generates git repo in git (initiates readme and clone link)
-//step 2: user navigates to local directory and runs git clone in terminal 
-//step 3: user gets the repo with a readme.md file
-//step 4: user adds the readme-maker files in the local and runs it in the terminal: answering prompts which get handled/formatted/and written to the local readme.
+/*
+INPUTS (USER)	INPUTS (SYSTEM)	OUTPUTS (USER)	OUTPUTS (sytem)
+username	FUNCTION to handle response for GITHUB USER NAME	a populated readme.md	generate .md file
+user email	FUNCTION to handle response for PROJECT TITLE		
+URL to project	FUNCTION to handle response for DESCRIPTION		
+project’s name	FUNCTION to handle response for ADD TABLE OF CONTENTS		
+short description	FUNCTION to handle response for ADD INSTALLATION INSTRUCTIONS?		
+license?	FUNCTION to handle response for CONTRIBUTOR LIST		
+cmd to install dependencies	FUNCTION to handle response for TESTS		
+cmd to run tests			
+additional things to know about repo?			
+additional contributors to repo?			*/
 
-//apporach 2
-//step 1: user generates git repo in git (initiates readme and clone link)
-//step 2: before cloning the repo, user runs readme-maker files in terminal and answers questions 
-//step 3: answers get handled/formatted/ and written to the git repo in git automatically
-//step 4: user can clone repo locally and already populated readme gets pulled down with first clone
+//uml chart - uses multiple columsn with actors involved - documentation
+
+/*approach 3: 
+//user runs index.js from terminal for readme-maker app
+//system takes in data and formats output
+//a function is called to populate the .md file
+//user can use the populated readme however they like */
 
 
 
-*/
+
+
+
+
