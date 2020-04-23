444aa7e82753175a59f9eca6f2e277fd0129615d
Claudia Petty <claudialove32@gmail.com>
2020-04-23 16:57:41 -0400

    added algorithms for use cases to better understand workflow for this app - 2 approaches, not sure which is right

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+
+// -- They give us an ARRAY called 'questions' What could we do with this (?) -- //
 const questions = [
 
 ];
 
+// -- They give us a writeToFile() FUNCTION, Looks like we may need to read/write to a file. What BUILT-IN node module will help us out with this (?) -- // 
 function writeToFile(fileName, data) {
 }
 
+// -- This is a fairly common programming construct. They are just giving us a FUNCTION to INITIALIZE or SETUP our project parameter. It's also where we usually kick off our project flow -- //
 function init() {
 
 }
 
+// -- We DEFINED our INITALIZATION FUNCTION above, here we are just kicking off (running) our program. -- // 
 init();
+
+
+//adding possible algorithim for workflow if this program ends up working as expected to understand where in the process it gets called once it exists
+
+
+//approach 1
+//step 1: user generates git repo in git (initiates readme and clone link)
+//step 2: user navigates to local directory and runs git clone in terminal 
+//step 3: user gets the repo with a readme.md file
+//step 4: user adds the readme-maker files in the local and runs it in the terminal: answering prompts which get handled/formatted/and written to the local readme.
+
+//apporach 2
+//step 1: user generates git repo in git (initiates readme and clone link)
+//step 2: before cloning the repo, user runs readme-maker files in terminal and answers questions 
+//step 3: answers get handled/formatted/ and written to the git repo in git automatically
+//step 4: user can clone repo locally and already populated readme gets pulled down with first clone
