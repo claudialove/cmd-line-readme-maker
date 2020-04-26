d17c5ee9eac80cf905f2b2c43eca063a271c4736
Claudia Petty <claudialove32@gmail.com>
2020-04-26 12:10:20 -0400

    hand written code to satisfy baseline requirements of homework - proud of this output

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
-//varibales declared for npms
+//variables declared for npms
 var inquirer = require("inquirer");
 var fs = require('fs');
 var axios = require('axios');
-const questions = [ 
+ 
+//prompts for user input
+inquirer.prompt(
+[ 
     {
         type: "input",
         name: "gitUserName",
@@ -61,73 +64,28 @@ const questions = [
         message: "What does the user need to know about contributing to the repo?"
     }
 
-];
-
-//prompts for user input
-inquirer.prompt().then(function(data) {
-    //let output = empty string
-    let output = "";
-    output += `# ${data.gitProjectName}\n`
-    output += "\n"
-    output += `## ${data.gitProjectDesc}`
-    output += "\n"
-    output += `## ${data.gitUserName}`
-    output += "\n"
-    output += `## ${data.gitAssociatedEmail}`
-    output += "\n"
-    output += `## ${data.gitProjectUrl}`
-    output += "\n"
-    output += `## ${data.activities}`
-    output += "\n"
-    output += `## ${data.license}`
-    output += "\n"
-    output += `## ${data.installDepends}`
-    output += "\n"
-    output += `## ${data.runTests}`
-    output += "\n"
-    output += `## ${data.aboutRepo}`
-    output += "\n"
-    output += `## ${data.contributeToRepo}`
-  writeToFile("reame.md", output)
-    });
-    
-// -- They give us a writeToFile() FUNCTION, Looks like we may need to read/write to a file. What BUILT-IN node module will help us out with this (?) -- // 
-function writeToFile(fileName, data) {
-    // use fs to output the (google fs write to file)
-}
-
-// -- This is a fairly common programming construct. They are just giving us a FUNCTION to INITIALIZE or SETUP our project parameter. It's also where we usually kick off our project flow -- //
-function init() {
-
-}
-
-// -- We DEFINED our INITALIZATION FUNCTION above, here we are just kicking off (running) our program. -- // 
-init();
+])
+//translates response into an object (use console log to test)
+.then(function(response) {
+    //make an object which is equal to the response data for project name and add top level formatting
+    let output = `# Project Name \n ${response.gitProjectName} \n \n` +
+    `## Project Description \n ${response.gitProjectDesc} \n \n` +
+    `## Author \n ${response.gitUserName} \n \n` +
+    `## Author Email \n ${response.gitAssociatedEmail} \n \n` +
+    `## Project Link \n ${response.gitProjectUrl} \n \n` +
+    `## License \n ${response.license} \n \n` +
+    `## Dependencies \n ${response.installDepends} \n \n` +
+    `## Tests \n ${response.runTests} \n \n` +
+    `## About Repo \n ${response.aboutRepo} \n \n` +
+    `## Contributions \n ${response.contributeToRepo} \n \n`; +
+    //write this data to the file
+    fs.writeFileSync('readme.md', output);
+});
 
 
-//adding possible algorithim for workflow if this program ends up working as expected to understand where in the process it gets called once it exists
 
 
-/*
-INPUTS (USER)	INPUTS (SYSTEM)	OUTPUTS (USER)	OUTPUTS (sytem)
-username	FUNCTION to handle response for GITHUB USER NAME	a populated readme.md	generate .md file
-user email	FUNCTION to handle response for PROJECT TITLE		
-URL to project	FUNCTION to handle response for DESCRIPTION		
-project’s name	FUNCTION to handle response for ADD TABLE OF CONTENTS		
-short description	FUNCTION to handle response for ADD INSTALLATION INSTRUCTIONS?		
-license?	FUNCTION to handle response for CONTRIBUTOR LIST		
-cmd to install dependencies	FUNCTION to handle response for TESTS		
-cmd to run tests			
-additional things to know about repo?			
-additional contributors to repo?			*/
 
-//uml chart - uses multiple columsn with actors involved - documentation
-
-/*approach 3: 
-//user runs index.js from terminal for readme-maker app
-//system takes in data and formats output
-//a function is called to populate the .md file
-//user can use the populated readme however they like */
 
 
 
